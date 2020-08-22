d2030ae8653f7307bbf89b3b440b776b77e1f468
anddrews <anddrews@tut.by>
2020-08-22 08:57:16 +0300

    hexagones

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-//here will by JavaScript code ...
+var body = document.querySelector('body');
+
+function isTarget(element) {
+    var parent = element.parentElement;
+    if (parent) {
+        if (parent.classList.contains('target')) {
+            return true;
+        }
+
+        return isTarget(parent);
+    } 
+    
+    return false;
+}
+
+body.addEventListener('click', function(e){
+    var target = e.target.parentElement;
+    console.log(isTarget(target));
+});
